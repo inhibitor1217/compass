3c56f07db124042c978b809d976547bc9dd26c1d
Dongwook Hwang <cytops1217@gmail.com>
2019-02-04 15:26:10 +0900

    minor tweak on walking logic

--- src/tester/PlayerMovement.java
+++ src/tester/PlayerMovement.java
@@ -2,17 +2,24 @@ package tester;
 
 import component.*;
 import engine.Timer;
+
 import input.KeyboardHandler;
 
 public class PlayerMovement extends Component {
 
+	private static final String WALK_ANIMATION = "walk1";
+	private static final String IDLE_ANIMATION = "stand1";
+	
+	private static final int WALK_STATE_NONE = 0;
+	private static final int WALK_STATE_LEFT = 1;
+	private static final int WALK_STATE_RIGHT = 2;
+	
 	private Transform2D transform;
 	private Animator animator;
 	
 	private final float v = .1f;
 	
-	private static final String WALK_ANIMATION = "walk1";
-	private static final String IDLE_ANIMATION = "stand1";
+	private int prevWalkState = WALK_STATE_NONE;
 	
 	@Override
 	public void awake() {
@@ -32,22 +39,34 @@ public class PlayerMovement extends Component {
 	public void update() {
 		// TODO Auto-generated method stub
 		if (KeyboardHandler.isKeyDown(KeyboardHandler.KEY_LEFT)) {
-			transform.translate(-v * Timer.deltaTime(), 0);
-			if (!animator.getCurrentAnimation().equals(WALK_ANIMATION)) {
-				animator.setCurrentAnimation(WALK_ANIMATION);
+			if (KeyboardHandler.isKeyDown(KeyboardHandler.KEY_RIGHT)) {
+				if (prevWalkState == WALK_STATE_LEFT)
+					walk(false);
+				else
+					walk(true);
 			}
-			animator.setMirror(false);
+			else
+				walk(false);
 		}
-		else if (KeyboardHandler.isKeyDown(KeyboardHandler.KEY_RIGHT)) {
-			transform.translate(v * Timer.deltaTime(), 0);
-			if (!animator.getCurrentAnimation().equals(WALK_ANIMATION)) {
-				animator.setCurrentAnimation(WALK_ANIMATION);
-			}
-			animator.setMirror(true);
-		}
-		else if (!animator.getCurrentAnimation().equals(IDLE_ANIMATION))
+		else if (KeyboardHandler.isKeyDown(KeyboardHandler.KEY_RIGHT))
+			walk(true);
+		else if (!animator.getCurrentAnimation().equals(IDLE_ANIMATION)) {
 			animator.setCurrentAnimation(IDLE_ANIMATION);
-		
+			prevWalkState = WALK_STATE_NONE;
+		}
+	}
+	
+	private void walk(boolean direction) {
+		// direction == false : walk left
+		// direction == true  : walk right
+		if (!animator.getCurrentAnimation().equals(WALK_ANIMATION)) {
+			animator.setCurrentAnimation(WALK_ANIMATION);
+		}
+		animator.setMirror(direction);
+
+		transform.translate((direction ? 1.0f : -1.0f) * v * Timer.deltaTime(), 0);
+
+		prevWalkState = direction ? WALK_STATE_RIGHT : WALK_STATE_LEFT;
 	}
 
 }
